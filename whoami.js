881b08578ea8d304ea909865c985a9497a007690
Stephen Rodriguez <steprodriguez10@gmail.com>
2015-07-05 18:07:54 -0400

    updated whoami for windows OS

--- whoami.js
+++ whoami.js
@@ -3,6 +3,9 @@
 
 	module.exports = function() {
 		var username = require('child_process').execSync('whoami', { encoding: 'utf-8' });
+		if(process.platform === 'win32') {
+			username = username.split(path.sep)[1];
+		}
 		return String(username).trim();
 	};
 })();
