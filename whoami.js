5bc6d79a96124dc7c6bf699dbbe2cd82d8d7e3c9
Stephen Rodriguez <steprodriguez10@gmail.com>
2015-07-24 23:47:06 -0400

    short desc added

--- whoami.js
+++ whoami.js
@@ -1,6 +1,7 @@
 (function(){
 	'use strict';
 
+	// Returns the name of the logged in user (useful for locating the user folder)
 	module.exports = function() {
 		var username = require('child_process').execSync('whoami', { encoding: 'utf-8' });
 		return String(username).trim();
