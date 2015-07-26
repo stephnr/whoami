9b6968037e5daf8b6da438c2d4f406dace85321d
Stephen Rodriguez <steprodriguez10@gmail.com>
2015-07-25 22:56:14 -0400

    reverting to general use

--- whoami.js
+++ whoami.js
@@ -1,12 +1,15 @@
 (function(){
 	'use strict';
 
+	var path = require('path');
+
+	var M;
+
 	// Returns the name of the logged in user (useful for locating the user folder)
-	module.exports = function() {
+	module.exports = M = function() {
 		var username = require('child_process').execSync('whoami', { encoding: 'utf-8' });
-		if(process.platform === 'win32') {
-			username = username.split(path.sep)[1];
-		}
 		return String(username).trim();
 	};
+
+	console.log(M());
 })();
