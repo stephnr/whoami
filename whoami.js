709544a5fe52d7a7a06a78d749c84357ed383dfd
Stephen Rodriguez <steprodriguez10@gmail.com>
2015-07-25 23:26:32 -0400

    removed log

--- whoami.js
+++ whoami.js
@@ -3,13 +3,9 @@
 
 	var path = require('path');
 
-	var M;
-
 	// Returns the name of the logged in user (useful for locating the user folder)
-	module.exports = M = function() {
+	module.exports = function() {
 		var username = require('child_process').execSync('whoami', { encoding: 'utf-8' });
 		return String(username).trim();
 	};
-
-	console.log(M());
 })();
